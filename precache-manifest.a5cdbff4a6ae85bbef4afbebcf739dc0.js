03b7e1c4c9712a9c79a01d62ea95d82b5a04772c
b.lajtar <bartolineon@o2.pl>
2020-10-16 17:28:09 +0200

    Updates

---
+++ precache-manifest.a5cdbff4a6ae85bbef4afbebcf739dc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1df11d303bd2631d12504c925217be8",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "d9be0b33e11582206243",
+    "url": "/todoApp/static/css/main.d3b18f39.chunk.css"
+  },
+  {
+    "revision": "81594e0560e7903f3d01",
+    "url": "/todoApp/static/js/2.8c64e5be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.8c64e5be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9be0b33e11582206243",
+    "url": "/todoApp/static/js/main.7f40eae3.chunk.js"
+  },
+  {
+    "revision": "8d72b9e18fd9ba1ca521",
+    "url": "/todoApp/static/js/runtime-main.46a836fe.js"
+  }
+]);
